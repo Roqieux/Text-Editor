5212d3497adac187a2c095eb916681c09638d5dd
larroquejl@gmail.com <larroquejl@gmail.com>
2023-10-15 22:58:32 -0400

    instantiated imports in weback.config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -11,13 +11,49 @@ module.exports = () => {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+      database: './src/js/database.js',
+      editor: './src/js/editor.js',
+      header: './src/js/header.js',
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+
+      // Generate HTML file and inject bundles
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE'
+      }),
+
+      // Inject custom service wookie
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
+      // Create manifest.json
+
+      new WebpackPwaManifest({
+        fingerprints: false,
+        Inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Take notes in the browser!',
+        background_color: '#393433',
+        theme_color: '#393433',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
       
     ],
 
